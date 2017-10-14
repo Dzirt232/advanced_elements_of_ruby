2c940fb71348f1e4635a6003a76816cc3e96b11a
Dzirt232 <dzirt232@rambler.ru>
2017-10-15 01:01:38 +0300

    bubble_sort basic created

---
+++ bubble_sort.rb
@@ -0,0 +1,15 @@
+def bubble_sort(array)
+	for i in 0..(array.length-1)
+		for j in 0..(array.length-i-2)
+			array[j], array[j+1] = array[j+1], array[j] if array[j]>array[j+1]
+		end
+	end
+	puts array.inspect 
+end
+
+def bubble_sort_by(array)
+	
+end
+
+bubble_sort([1,41,32,5,6,2,7])
+bubble_sort([1,41,32,5,6,2,5,34,7])
